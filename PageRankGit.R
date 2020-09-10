3338f2946abb2d18ab463b0f1fa6aa1df6777ad6
PG989 <paloczig@gmail.com>
2020-09-10 12:55:39 +0200

    Update PageRankGit.R

--- PageRankGit.R
+++ PageRankGit.R
@@ -3,15 +3,15 @@ PageRank <- function(pdb = ""){
   #library(igraph)
   
   # pdb <- fromJSON(jsonInput)
-  # g <- pdb[, "Package"] %>%
-  #   makeDepGraph(availPkgs = pdb, suggests=FALSE, enhances=TRUE, includeBasePkgs = FALSE)
-  #   
-  #   pr <- g %>%
-  #     page.rank(directed = FALSE) %>%
-  #     use_series("vector") %>%
-  #     sort(decreasing = TRUE) %>%
-  #     as.matrix %>%
-  #     set_colnames("page.rank")
+   g <- pdb[, "Package"] %>%
+     makeDepGraph(availPkgs = pdb, suggests=FALSE, enhances=TRUE, includeBasePkgs = FALSE)
+     
+     pr <- g %>%
+       page.rank(directed = FALSE) %>%
+       use_series("vector") %>%
+       sort(decreasing = TRUE) %>%
+       as.matrix %>%
+       set_colnames("page.rank")
     
     
     message = paste(pdb[1,1])

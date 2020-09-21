55d56e331210e3b8119f763068d293ef93f8fe11
PG989 <paloczig@gmail.com>
2020-09-21 15:20:36 +0200

    Update PageRankGit.R

--- PageRankGit.R
+++ PageRankGit.R
@@ -1,9 +1,20 @@
-PageRank <- function(pdb = ""){
-  #install.packages('igraph')
-  #library(igraph)
+#setwd('p:/_Users/_PGabi/PageRank/')
+
+library(igraph)
+library(jsonlite)
+library(miniCRAN)
+library(magrittr)
+#readed_data <- fromJSON("test.json")
+
+# GitHub  ----------------------------------
+
+PageRank <- function(file, ...){
+
   
-  # pdb <- fromJSON(jsonInput)
-   g <- pdb[, "Package"] %>%
+   pdb <- as.data.frame(fromJSON(file))
+   colnames(pdb) <- c("Package", "Version", "Priority",  "Depends", "Imports", "LinkingTo", "Suggests", "Enhances",  "License", "License_is_FOSS", "License_restricts_use", "OS_type", "Archs", "MD5sum", "NeedsCompilation", "File", "Repository")
+   rownames(pdb) <- pdb$Package
+   g <- pdb[, 1] %>%
      makeDepGraph(availPkgs = pdb, suggests=FALSE, enhances=TRUE, includeBasePkgs = FALSE)
      
      pr <- g %>%
@@ -12,8 +23,9 @@ PageRank <- function(pdb = ""){
        sort(decreasing = TRUE) %>%
        as.matrix %>%
        set_colnames("page.rank")
-    
-    
-    message = paste(pdb[1,1])
-    
+     
+     pr <- as.data.frame(pr)
+     pr$Package <- rownames(pr)
+
+  return(toJSON(pr[1:10,]))          
 }
